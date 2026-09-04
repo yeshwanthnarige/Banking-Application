d23e831b713abaf1a957c36002c1af23125a363d
Yeshwanth Narige <61676569+yeshwanthnarige@users.noreply.github.com>
2026-09-04 16:02:12 -0500

    Align database schema and initialization

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,12 @@
 CREATE SCHEMA online_bank;
 
+CREATE SEQUENCE online_bank.account_sequence START WITH 3;
 CREATE TABLE online_bank.account (
     id bigint NOT NULL PRIMARY KEY,
-    sort_code CHAR(8) NOT NULL,
-    account_number CHAR(8) NOT NULL,
-    current_balance NUMERIC(10,3) NOT NULL,
+    version bigint DEFAULT 0 NOT NULL,
+    sort_code VARCHAR(8) NOT NULL,
+    account_number VARCHAR(8) NOT NULL,
+    current_balance NUMERIC(19,2) NOT NULL,
     bank_name VARCHAR(50) NOT NULL,
     owner_name VARCHAR(50) NOT NULL,
     UNIQUE (sort_code, account_number)
@@ -17,10 +19,10 @@ CREATE TABLE online_bank.transaction (
     target_account_id bigint NOT NULL REFERENCES online_bank.account(id),
     -- Partially denormalize for performance
     target_owner_name varchar(50) NOT NULL,
-    amount NUMERIC(10,3) NOT NULL,
+    amount NUMERIC(19,2) NOT NULL,
     initiation_date timestamp NOT NULL,
     completion_date TIMESTAMP,
     reference VARCHAR(255),
-    latitude REAL,
-    longitude REAL
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION
 );
